84254bf3f255d8ddede8741fa282da3b536d8ae9
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-07-01 01:13:04 +0600

    create error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { FaRegGrinAlt } from "react-icons/fa";
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
-    return (
-        <div>
-            ErrorPage
+  const { error, status } = useRouteError()
+  return (
+    <div className='flex items-center p-16 '>
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+       
+        <FaRegGrinAlt className='w-40 h-40 text-red-600' />
+        <div className='max-w-md text-center'>
+          <h2 className='mb-8 font-extrabold text-9xl text-indigo-500'>
+            <span className='pe-5'>Error</span>
+            {status || 404}
+          </h2>
+          <p className='text-2xl font-semibold md:text-3xl text-red-800 mb-8'>
+            {error?.message}
+          </p>
+          <Link to='/' className=' text-white bg-blue-500 py-2 px-3 rounded-md'>
+            Back to homepage
+          </Link>
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
 export default ErrorPage;
